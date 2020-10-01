b80a19a02cf37a1351eef068337f5dc06e1efdb7
SU3K5T <surikat0013@icloud.com>
2020-10-01 18:29:31 +0300

    create: map with using yandex API

--- js/main.js
+++ js/main.js
@@ -15,3 +15,33 @@ var mySwiper = new Swiper(".swiper-container", {
 
     speed: 500,
 });
+
+ymaps.ready(function () {
+    var map = new ymaps.Map("map", {
+        center: [7.573638, 79.803867],
+        zoom: 13,
+        controls: [
+            "geolocationControl",
+            "zoomControl",
+            "routeButtonControl",
+            "fullscreenControl",
+            "typeSelector",
+        ],
+    });
+    control = map.controls.get("routeButtonControl");
+
+    var location = ymaps.geolocation.get();
+
+    location.then(function (res) {
+        var userTextLocation = res.geoObjects.get(0).properties.get("text");
+        control.routePanel.state.set({
+            from: userTextLocation,
+
+            to: [7.573638, 79.803867],
+        });
+    });
+    var myPlacemark = new ymaps.Placemark([7.573638, 79.803867], {
+        hintContent: "Grand Hilton Hotel",
+    });
+    map.geoObjects.add(myPlacemark);
+});
