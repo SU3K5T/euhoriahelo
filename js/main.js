bbf6258a18ce6a1b7144b7a2f4ce382ff560139f
SU3K5T <surikat0013@icloud.com>
2020-10-17 20:38:10 +0300

    create: validate

--- js/main.js
+++ js/main.js
@@ -94,6 +94,26 @@ menuButton.addEventListener("click", function () {
              
     }
 
+    // form validation
 
-
+    $('.form').each(function (){
+        $(this).validate({
+         errorClass: "invalid",
+         messages: {
+            name: {
+                required: "Name is required",
+                minlength: "Name must be at least 2 characters",
+            } ,
+            email: {
+                required: "Email is required",
+                email: "Format: name@domain.com"
+            },
+            phone: {
+                required: "Phone is required",
+            }
+        }
+    });
+    })
+   $('.phone_with_ddd').mask('+7 (999) 999-99-99');
+    
 });
